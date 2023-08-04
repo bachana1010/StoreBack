68a66d4d1159f69a8153a66fac62387429379663
bacho <b@mail.com>
2023-08-04 10:01:20 +0400

    adddeletebranchanduser

---
+++ Migrations/20230803134019_deleteuserupdate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class deleteuserupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+             migrationBuilder.Sql(@"
+                CREATE OR ALTER PROCEDURE DeleteBranch
+                    @Id int
+
+                AS
+                BEGIN
+                    -- Soft delete the users associated with this branch
+                    UPDATE Users
+                    SET DeletedAt = GETDATE()
+                    WHERE BranchId = @Id;                
+
+                    -- Soft delete the branch
+                    UPDATE Branches
+                    SET DeletedAt = GETDATE()
+                    WHERE Id = @Id;
+                END
+            ");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+                        migrationBuilder.Sql("DROP PROCEDURE deleteuserupdate");
+
+
+        }
+    }
+}
